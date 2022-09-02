4bdcd14e22810658292cd5da36e822102a5ee9a6
anemharikrishna@gmail.com <anemharikrishna@gmail.com>
2022-09-02 15:27:07 +0530

    navbar

--- pages/index.js
+++ pages/index.js
@@ -14,13 +14,23 @@ export default function Home() {
 
       <div className="flex flex-col min-h-screen justify-between flex-wrapper">
         <header>
-          <nav className="items-center justify-between flex-wrap bg-black p-6">
+          {/* <nav className="items-center justify-between flex-wrap bg-black p-6">
             <div className="text-center items-center flex-shrink-0 text-white">
               <span className="font-semibold text-xl tracking-tight leading-1">NextJS Serverless CRUD Opeations</span>
             </div>
             <div className="text-right items-center flex-shrink-0 text-white float-right addProductBtn">
               <span className="font-semibold text-xl tracking-tight"><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add a product</button>
               </span>
+            </div>
+          </nav> */}
+
+
+          <nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+            <div class="container flex flex-wrap justify-between items-center mx-auto">
+              <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">NextJS Serverless CRUD Opeations</span>
+              <div class="flex md:order-2">
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add a product</button>
+              </div>
             </div>
           </nav>
         </header>
