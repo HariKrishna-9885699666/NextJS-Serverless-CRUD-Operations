3bc338eb08b445561a0b4633e588d897c1f8ae0f
anemharikrishna@gmail.com <anemharikrishna@gmail.com>
2022-09-02 13:24:25 +0530

    fix css issues

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
               <span className="font-semibold text-xl tracking-tight leading-1">NextJS Serverless CRUD Opeations</span>
             </div>
             <div className="text-right items-center flex-shrink-0 text-white float-right addProductBtn">
-              <span className="font-semibold text-xl tracking-tight"><button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add a product</button>
+              <span className="font-semibold text-xl tracking-tight"><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add a product</button>
               </span>
             </div>
           </nav>
@@ -32,7 +32,7 @@ export default function Home() {
               <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                 <a href="#" className="group">
                   <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                    <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." className="h-full w-full object-cover object-center group-hover:opacity-75" />
+                    <Image src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." className="h-full w-full object-cover object-center group-hover:opacity-75" />
                   </div>
                   <h3 className="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
                   <p className="mt-1 text-lg font-medium text-gray-900">$48</p>
@@ -40,7 +40,7 @@ export default function Home() {
 
                 <a href="#" className="group">
                   <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                    <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg" alt="Olive drab green insulated bottle with flared screw lid and flat top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
+                    <Image src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg" alt="Olive drab green insulated bottle with flared screw lid and flat top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
                   </div>
                   <h3 className="mt-4 text-sm text-gray-700">Nomad Tumbler</h3>
                   <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
@@ -48,7 +48,7 @@ export default function Home() {
 
                 <a href="#" className="group">
                   <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                    <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg" alt="Person using a pen to cross a task off a productivity paper card." className="h-full w-full object-cover object-center group-hover:opacity-75" />
+                    <Image src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg" alt="Person using a pen to cross a task off a productivity paper card." className="h-full w-full object-cover object-center group-hover:opacity-75" />
                   </div>
                   <h3 className="mt-4 text-sm text-gray-700">Focus Paper Refill</h3>
                   <p className="mt-1 text-lg font-medium text-gray-900">$89</p>
@@ -56,7 +56,7 @@ export default function Home() {
 
                 <a href="#" className="group">
                   <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                    <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
+                    <Image src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
                   </div>
                   <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
                   <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
@@ -64,7 +64,7 @@ export default function Home() {
 
                 <a href="#" className="group">
                   <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                    <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
+                    <Image src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
                   </div>
                   <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
                   <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
@@ -72,7 +72,7 @@ export default function Home() {
 
                 <a href="#" className="group">
                   <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                    <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
+                    <Image src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
                   </div>
                   <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
                   <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
@@ -80,7 +80,7 @@ export default function Home() {
 
                 <a href="#" className="group">
                   <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                    <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
+                    <Image src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
                   </div>
                   <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
                   <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
@@ -88,7 +88,7 @@ export default function Home() {
 
                 <a href="#" className="group">
                   <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                    <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
+                    <Image src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
                   </div>
                   <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
                   <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
@@ -96,7 +96,7 @@ export default function Home() {
 
                 <a href="#" className="group">
                   <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                    <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
+                    <Image src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75" />
                   </div>
                   <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
                   <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
@@ -104,9 +104,9 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div class="relative h-32 w-32 float-right userIcon">
-            <div class="absolute bottom-0 right-0 h-16 w-16">
-              <Image src={userPic} onClick={() => {
+          <div className="relative h-32 w-32 float-right userIcon">
+            <div className="absolute bottom-0 right-0 h-16 w-16">
+              <Image src={userPic} alt="About App" onClick={() => {
               }} />
             </div>
           </div>
