631e86b25b04c36732214f856016507ac75de9ce
anemharikrishna@gmail.com <anemharikrishna@gmail.com>
2022-09-06 18:03:20 +0530

    class name

--- components/Product/Add.js
+++ components/Product/Add.js
@@ -169,7 +169,7 @@ function AddProduct({ loader, setLoader } = props) {
                         ease-in-out">Submit</button>
                         &nbsp;
                         <Link href="/">
-                            <button type="button" class="inline-block px-6 py-2.5 bg-gray-200 text-gray-700 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out">Back</button>
+                            <button type="button" className="inline-block px-6 py-2.5 bg-gray-200 text-gray-700 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out">Back</button>
                         </Link>
                     </form>
                 </div>
