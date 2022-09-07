6a2a536d26379acc8bb88c0187acd0b400ba8c8a
Hari Krishna Anem <anemharikrishna@gmail.com>
2022-09-07 19:02:12 +0530

    Update Layout.js

--- components/shared/Layout.js
+++ components/shared/Layout.js
@@ -9,7 +9,7 @@ function Layout({ children, open, showModal, setShowModal, handleOpen } = props)
                     <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
                         <div className="container flex flex-wrap justify-between items-center mx-auto">
                         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                            <Link href="/">NextJS Serverless CRUD Opeations</Link>
+                            <Link href="/">NextJS Serverless CRUD Operations</Link>
                         </span>
                         <div className="flex md:order-2">
                             <Link href="/product/add">
